b9be73705017034c396bc119009dfc2400b96160
Suhan Moon <leader@trabricks.io>
2021-05-22 21:40:31 +0900

    init commit

---
+++ android/src/main/java/io/actbase/ivs/RIVSPlayerManager.java
@@ -0,0 +1,101 @@
+package io.actbase.ivs;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.Map;
+
+public class RIVSPlayerManager extends ViewGroupManager<RIVSPlayerView> {
+
+
+    private static final int COMMAND_PAUSE_ID = 1;
+    private static final String COMMAND_PAUSE_NAME = "pause";
+    private static final int COMMAND_PLAY_ID = 2;
+    private static final String COMMAND_PLAY_NAME = "play";
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "RIVSPlayer";
+    }
+
+    @NonNull
+    @Override
+    protected RIVSPlayerView createViewInstance(@NonNull ThemedReactContext reactContext) {
+        RIVSPlayerView view = new RIVSPlayerView(reactContext);
+        return view;
+    }
+
+    @Override
+    public Map getExportedCustomBubblingEventTypeConstants() {
+        return MapBuilder.builder()
+                .put("topChangeState", MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onChangeState")))
+                .put("topChangeVideoSize", MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onChangeVideoSize")))
+                .put("topFailWithError", MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onFailWithError")))
+                .put("topOutputCue", MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onOutputCue")))
+                .build();
+    }
+
+    @ReactProp(name = "uri")
+    public void setUri(RIVSPlayerView view, @Nullable String inputUrl) {
+        view.setUri(inputUrl);
+    }
+
+    @ReactProp(name = "autoPlay")
+    public void setAutoPlay(RIVSPlayerView view, Boolean autoPlay) {
+        view.setAutoPlay(autoPlay);
+    }
+
+    @ReactProp(name = "scaleMode")
+    public void setScaleMode(RIVSPlayerView view, String mode) {
+        view.setScaleMode(mode);
+    }
+
+    @ReactProp(name = "liveLowLatencyEnabled")
+    public void setLiveLowLatencyEnabled(RIVSPlayerView view, Boolean autoPlay) {
+        view.setLiveLowLatencyEnabled(autoPlay);
+    }
+
+    @ReactProp(name = "muted")
+    public void setMuted(RIVSPlayerView view, Boolean muted) {
+        view.setMuted(muted);
+    }
+
+    @Nullable
+    @Override
+    public Map<String, Integer> getCommandsMap() {
+        return MapBuilder.of(
+                COMMAND_PAUSE_NAME, COMMAND_PAUSE_ID,
+                COMMAND_PLAY_NAME, COMMAND_PLAY_ID
+                );
+    }
+
+    @Override
+    public void receiveCommand(RIVSPlayerView root, int commandId, @Nullable ReadableArray args) {
+        switch (commandId) {
+            case COMMAND_PAUSE_ID:
+                root.pause();
+                break;
+            case COMMAND_PLAY_ID:
+                root.play();
+                break;
+        }
+    }
+
+//    RCT_EXPORT_VIEW_PROPERTY(uri, NSString)
+//    RCT_EXPORT_VIEW_PROPERTY(autoPlay, BOOL)
+//    RCT_EXPORT_VIEW_PROPERTY(scaleMode, NSString)
+//    RCT_EXPORT_VIEW_PROPERTY(liveLowLatencyEnabled, BOOL)
+//    RCT_EXPORT_VIEW_PROPERTY(muted, BOOL)
+//    RCT_EXPORT_VIEW_PROPERTY(onChangeState, RCTBubblingEventBlock)
+//    RCT_EXPORT_VIEW_PROPERTY(onChangeVideoSize, RCTBubblingEventBlock)
+//    RCT_EXPORT_VIEW_PROPERTY(onFailWithError, RCTBubblingEventBlock)
+//    RCT_EXPORT_VIEW_PROPERTY(onOutputCue, RCTBubblingEventBlock)
+
+}
---
+++ android/src/main/java/io/actbase/ivs/RIVSPlayerView.java
@@ -0,0 +1,172 @@
+package io.actbase.ivs;
+
+import android.content.Context;
+import android.net.Uri;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+
+import com.amazonaws.ivs.player.Cue;
+import com.amazonaws.ivs.player.Player;
+import com.amazonaws.ivs.player.PlayerException;
+import com.amazonaws.ivs.player.PlayerView;
+import com.amazonaws.ivs.player.Quality;
+import com.amazonaws.ivs.player.Releasable;
+import com.amazonaws.ivs.player.TextMetadataCue;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+public class RIVSPlayerView extends FrameLayout {
+
+    private PlayerView playerView;
+    private boolean autoPlay = true;
+
+    public RIVSPlayerView(@NonNull Context context) {
+        super(context);
+
+        playerView = new PlayerView(context);
+        playerView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        playerView.setBackgroundColor(getResources().getColor(android.R.color.black));
+        playerView.setControlsEnabled(false);
+        this.addView(playerView);
+
+        Player player = playerView.getPlayer();
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onCue(@NonNull Cue cue) {
+                TextMetadataCue textMetadataCue = (TextMetadataCue) cue;
+                if (textMetadataCue != null) {
+                    WritableMap event = Arguments.createMap();
+                    event.putInt("code", 0);
+                    event.putString("msg", textMetadataCue.text);
+                    ReactContext reactContext = (ReactContext) getContext();
+                    reactContext.getJSModule(RCTEventEmitter.class)
+                            .receiveEvent(
+                                    getId(),
+                                    "topOutputCue",
+                                    event);
+                }
+            }
+
+            @Override
+            public void onDurationChanged(long l) {
+
+            }
+
+            @Override
+            public void onStateChanged(@NonNull Player.State state) {
+                ReactContext reactContext = (ReactContext) getContext();
+                RCTEventEmitter emitter = reactContext.getJSModule(RCTEventEmitter.class);
+                switch (state) {
+                    case IDLE: {
+                        WritableMap event = Arguments.createMap();
+                        event.putInt("code", 0);
+                        event.putString("msg", "IVSPlayerStateIdle");
+                        emitter.receiveEvent(getId(), "topChangeState", event);
+                        break;
+                    }
+                    case READY: {
+                        WritableMap event = Arguments.createMap();
+                        event.putInt("code", 1);
+                        event.putString("msg", "IVSPlayerStateReady");
+                        emitter.receiveEvent(getId(), "topChangeState", event);
+                        if (RIVSPlayerView.this.autoPlay) {
+                            Player player = playerView.getPlayer();
+                            player.play();
+                        }
+                        break;
+                    }
+                    case BUFFERING: {
+                        WritableMap event = Arguments.createMap();
+                        event.putInt("code", 2);
+                        event.putString("msg", "IVSPlayerStateBuffering");
+                        emitter.receiveEvent(getId(), "topChangeState", event);
+                        break;
+                    }
+                    case PLAYING: {
+                        WritableMap event = Arguments.createMap();
+                        event.putInt("code", 4);
+                        event.putString("msg", "IVSPlayerStatePlaying");
+                        emitter.receiveEvent(getId(), "topChangeState", event);
+                        break;
+                    }
+                    case ENDED: {
+                        WritableMap event = Arguments.createMap();
+                        event.putInt("code", 5);
+                        event.putString("msg", "IVSPlayerStateEnded");
+                        emitter.receiveEvent(getId(), "topChangeState", event);
+                        break;
+
+                    }
+                }
+            }
+
+            @Override
+            public void onError(@NonNull PlayerException e) {
+
+            }
+
+            @Override
+            public void onRebuffering() {
+
+            }
+
+            @Override
+            public void onSeekCompleted(long l) {
+
+            }
+
+            @Override
+            public void onVideoSizeChanged(int w, int h) {
+
+                WritableMap event = Arguments.createMap();
+                event.putInt("code", 0);
+                event.putInt("width", w);
+                event.putInt("height", h);
+                ReactContext reactContext = (ReactContext) getContext();
+                reactContext.getJSModule(RCTEventEmitter.class)
+                        .receiveEvent(
+                                getId(),
+                                "topOutputCue",
+                                event);
+            }
+
+            @Override
+            public void onQualityChanged(@NonNull Quality quality) {
+
+            }
+        });
+    }
+
+    public void setUri(String uri) {
+        Player player = playerView.getPlayer();
+        player.load(Uri.parse(uri));
+    }
+
+    public void setAutoPlay(Boolean autoPlay) {
+        this.autoPlay = autoPlay;
+    }
+
+    public void setScaleMode(String mode) {
+    }
+
+    public void setLiveLowLatencyEnabled(Boolean autoPlay) {
+        playerView.getPlayer().setLiveLowLatencyEnabled(autoPlay);
+    }
+
+    public void setMuted(Boolean muted) {
+        playerView.getPlayer().setMuted(muted);
+    }
+
+    public void play() {
+        playerView.getPlayer().play();
+    }
+
+    public void pause() {
+        playerView.getPlayer().pause();
+    }
+
+}
