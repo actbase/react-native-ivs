bcdb79667d56d50942562222bf448620245f6277
Suhan Moon <leader@trabricks.io>
2021-05-22 15:04:53 +0900

    init commit

--- PlayerView.js
+++ PlayerView.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Text, View, ViewPropTypes} from "react-native";
 
-const PlayerView = (props) => (
-    <></>
-);
+const PlayerView = (props) => {
+  const { source, ...viewProps} = props;
+  return (
+      <View {...viewProps}>
+        <Text>video..</Text>
+      </View>
+  );
+}
 
+PlayerView.propTypes = ViewPropTypes;
 export default PlayerView;
