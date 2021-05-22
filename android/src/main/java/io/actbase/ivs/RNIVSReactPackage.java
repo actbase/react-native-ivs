2f7ee1295e8ff241f234fa970d8c79aa63f73bae
Suhan Moon <leader@trabricks.io>
2021-05-22 20:48:03 +0900

    init commit

--- android/src/main/java/io/actbase/ivs/RNIVSReactPackage.java
+++ android/src/main/java/io/actbase/ivs/RNIVSReactPackage.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class RNVISReactPackage implements ReactPackage{
+public class RNIVSReactPackage implements ReactPackage{
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
