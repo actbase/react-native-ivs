a176cbdc05dc5b3062b8c250c49986222d6bb860
Suhan Moon <leader@trabricks.io>
2021-05-22 23:07:31 +0900

    init commit

--- ios/RTMPCameraView.m
+++ ios/RTMPCameraView.m
@@ -6,6 +6,7 @@
 //
 
 #import "RTMPCameraView.h"
+#import "LFLiveKit.h"
 
 @interface RTMPCameraView()
 
@@ -18,9 +19,62 @@
 {
     self = [super init];
     if (self) {
-        
+        [self requestAccessForVideo];
+        [self requestAccessForAudio];
     }
     return self;
+}
+
+#pragma mark -- Public Method
+- (void)requestAccessForVideo {
+    __weak typeof(self) _self = self;
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    switch (status) {
+    case AVAuthorizationStatusNotDetermined: {
+        // 许可对话没有出现，发起授权许可
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [_self.session setRunning:YES];
+                    });
+                }
+            }];
+        break;
+    }
+    case AVAuthorizationStatusAuthorized: {
+        // 已经开启授权，可继续
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [_self.session setRunning:YES];
+        });
+        break;
+    }
+    case AVAuthorizationStatusDenied:
+    case AVAuthorizationStatusRestricted:
+        // 用户明确地拒绝授权，或者相机设备无法访问
+
+        break;
+    default:
+        break;
+    }
+}
+
+- (void)requestAccessForAudio {
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+    switch (status) {
+    case AVAuthorizationStatusNotDetermined: {
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
+            }];
+        break;
+    }
+    case AVAuthorizationStatusAuthorized: {
+        break;
+    }
+    case AVAuthorizationStatusDenied:
+    case AVAuthorizationStatusRestricted:
+        break;
+    default:
+        break;
+    }
 }
 
 @end
